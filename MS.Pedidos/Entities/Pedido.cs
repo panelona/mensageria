5edbca1625d670fb0c9be7febba4622c1d197530
Gabriela Calcabrine <gabrielacalcabrine@gmail.com>
2023-07-06 13:51:01 -0300

    :construction: feat/Criando Entidade Pedido

---
+++ MS.Pedidos/Entities/Pedido.cs
@@ -0,0 +1,16 @@
+﻿namespace MS.Pedidos.Entities
+{
+    public class Pedido
+    {
+        public Guid Id { get; init; } = new Guid();
+        public int NumeroPedido { get; set; }
+
+        public string Item { get; set; }
+
+        public string NumeroCartao { get; set; }
+
+        public string Cep { get; set; }
+
+        public string EmailCliente { get; set; }
+    }
+}
