254297f7770ea92bbbd86dee5d2257260ce0aa4d
unknown <pedro.silva@rarolabs.com.br>
2023-07-13 11:17:49 -0300

    chore: configuracao do projeto

--- MS.Pagamentos/Program.cs
+++ MS.Pagamentos/Program.cs
@@ -1,11 +1,24 @@
+using MS.Pagamentos.Domain.Interfaces;
+using MS.Pagamentos.Domain.Services;
+using MS.Pagamentos.Eventos;
+using MS.Pagamentos.RabbitMq;
+using System.Text.Json.Serialization;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddJsonOptions(
+        options => options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter())
+    );
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddHostedService<RabbitMqSubscriber>();
+builder.Services.AddScoped<IPagamentoService, PagamentoService>();
+builder.Services.AddSingleton<IProcessaEvento, ProcessaEvento>();
+builder.Services.AddSingleton<IRabbitMqClient, RabbitMqClient>();
+
 
 var app = builder.Build();
 
