901dbb90dfb0f13c0f68333484970eaa176e6d32
unknown <pedro.silva@rarolabs.com.br>
2023-07-13 11:14:55 -0300

    feat: add entidades do dominio

---
+++ MS.Pagamentos/Domain/Entities/DadosPagamentoEntitie.cs
@@ -0,0 +1,8 @@
+﻿namespace MS.Pagamentos.Domain.Entities
+{
+    public class DadosPagamentoEntitie
+    {
+        public string Email { get; set; }
+        public string NumCartao { get; set; }
+    }
+}
---
+++ MS.Pagamentos/Domain/Entities/RespostaPagamentoEntitie.cs
@@ -0,0 +1,13 @@
+﻿using MS.Pagamentos.Domain.Enums;
+using Newtonsoft.Json.Converters;
+using System.Text.Json.Serialization;
+
+namespace MS.Pagamentos.Domain.Entities
+{
+    public class RespostaPagamentoEntitie
+    {
+        public string Email { get; set; }
+
+        public StatusPagamento Status { get; set; }
+    }
+}
